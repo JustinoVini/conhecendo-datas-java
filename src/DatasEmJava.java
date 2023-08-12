c740cca624a93837ecfe23106b04b6135b643e5a
Vinicius Justino <visilva5690@hotmail.com>
2023-08-11 22:36:35 -0300

    datas

--- src/DatasEmJava.java
+++ src/DatasEmJava.java
@@ -3,7 +3,22 @@ import java.util.Date;
 public class DatasEmJava {
 
     public static void main(String[] args) {
-        Date     
+        Date date = new Date();
+
+        System.out.println("Datas em milisegundos " + date.getTime());
+
+        System.out.println("Dia do mês " + date.getDate());
+
+        System.out.println("Dia da semana " + date.getDay());
+
+        System.out.println("hora do dia " + date.getHours());
+
+        System.out.println("Minuto da hora " + date.getMinutes());
+
+        System.out.println("Segundos da hora " + date.getSeconds());
+
+        System.out.println("Ano " + (date.getYear() + 1900));
+
     }
 
 }
